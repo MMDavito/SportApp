ae3d3fe4ab5d42dbc71dd4e38f617362d2b8aab8
MMDavito <davidking9753@gmail.com>
2016-11-17 15:06:47 +0100

    Buggfix i längd (så fältet har en fast längd)

--- src/java/nu/te4/beans/SportsBean.java
+++ src/java/nu/te4/beans/SportsBean.java
@@ -6,14 +6,17 @@
 package nu.te4.beans;
 
 import com.mysql.jdbc.Connection;
+import java.io.StringReader;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.ejb.Stateless;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import nu.te4.support.ConnectionFactory;
-
 
 @Stateless
 public class SportsBean {
@@ -38,15 +41,100 @@ public class SportsBean {
                         .add("hemmalag", hl)
                         .add("bortalag", bl)
                         .add("poanghemma", hp)
-                        .add("poangbort", bp).build());
+                        .add("poangborta", bp).build());
 
             }
             connection.close();
-            
+
             return jsonArrayBuilder.build();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return null;
+    }
+
+    public boolean addGame(String body) {
+        System.out.println(body);
+        JsonReader jsonReader = Json.createReader(new StringReader(body));
+        System.out.println(body);
+        JsonObject data = jsonReader.readObject();
+
+        System.out.println("fuck");
+        jsonReader.close();
+
+        int hl = data.getInt("hemmalag");
+        int bl = data.getInt("bortalag");
+        int ph = data.getInt("poänghemma");
+        int pb = data.getInt("poängborta");
+        //regler
+        if ((ph + pb == 3) && (hl > 0 && hl <= 10) && (bl > 0 && bl <= 10)) {
+            try {
+                Connection connection = ConnectionFactory.make("testserver");
+                PreparedStatement stmt = connection.prepareStatement("INSERT INTO matcher VALUES(NULL,?,?,?,?");
+                stmt.setInt(1, hl);
+                stmt.setInt(2, bl);
+                stmt.setInt(3, ph);
+                stmt.setInt(4, pb);
+                stmt.executeUpdate();
+                connection.close();
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deleteGame(int id) {
+        try {
+            Connection connection = ConnectionFactory.make("testserver");
+            PreparedStatement stmt = connection.prepareStatement("DELETE FROM matcher WHERE id =?");
+            stmt.setInt(1, id);
+            stmt.execute();
+
+            connection.close();
+            return true;
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean changeGame(String body, int id) {
+        JsonReader jsonReader = Json.createReader(new StringReader(body));
+        System.out.println(body);
+        JsonObject data = jsonReader.readObject();
+
+        System.out.println("fuck");
+        jsonReader.close();
+
+        int hl = data.getInt("hemmalag");
+        int bl = data.getInt("bortalag");
+        int ph = data.getInt("poänghemma");
+        int pb = data.getInt("poängborta");
+        //regler
+        if ((ph + pb == 3) && (hl > 0 && hl <= 10) && (bl > 0 && bl <= 10)) {
+            try {
+                Connection connection = ConnectionFactory.make("testserver");
+                PreparedStatement stmt = connection.prepareStatement("UPDATE matcher\n"
+                        + "SET VALUES(NULL,?,?,?,?)\n"
+                        + "WHERE id =?");
+                stmt.setInt(1, hl);
+                stmt.setInt(2, bl);
+                stmt.setInt(3, ph);
+                stmt.setInt(4, pb);
+                stmt.setInt(5, id);
+                stmt.execute();
+                connection.close();
+                return true;
+            }catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            return false;
+        }
+        }else{
+            System.out.println("Fuck");
+                    return false;}
+    
     }
 }
--- src/java/nu/te4/services/SportService.java
+++ src/java/nu/te4/services/SportService.java
@@ -7,8 +7,13 @@ package nu.te4.services;
 
 import javax.ejb.EJB;
 import javax.json.JsonArray;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
@@ -31,9 +36,52 @@ public class SportService {
             return Response.status(401).build();
         }
         JsonArray data = sportsbean.getGames();
-        if(data ==null){
-        return Response.serverError().build();
+        if (data == null) {
+            return Response.serverError().build();
         }
         return Response.ok(data).build();
     }
+
+    @POST
+    @Path("game")
+    @Consumes(MediaType.APPLICATION_JSON)
+
+    public Response addGame(String body, @Context HttpHeaders httpHeaders) {
+        if (!User.authoricate(httpHeaders)) {
+            return Response.status(401).build();
+        }
+        if (sportsbean.addGame(body)) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @DELETE
+    @Path("game/{id}")
+    public Response deleteGame(@PathParam("id") int id, @Context HttpHeaders httpHeaders) {
+        if (!User.authoricate(httpHeaders)) {
+            return Response.status(401).build();
+        }
+        if (!sportsbean.deleteGame(id)) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path("game/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response changeGame(String body, @PathParam("id") int id, @Context HttpHeaders httpHeaders) {
+        if (!User.authoricate(httpHeaders)) {
+            return Response.status(401).build();
+        }
+        if (!sportsbean.deleteGame(id)) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        if (sportsbean.addGame(body)) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.status(Response.Status.CREATED).build();
+    }
+
 }
